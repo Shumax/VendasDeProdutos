a50f84ea40a7099eab160f7c4d7b122bd105a191
Shumax <maxcastro.mv@gmail.com>
2020-03-09 20:19:33 -0300

    front produtos

--- public/views/produtos.php
+++ public/views/produtos.php
@@ -1,13 +1,13 @@
 <?php
-
+/*
 use Buy\Controllers\ProdutosControl;
 
 require "../../vendor/autoload.php";
 require "../../src/Models/Produtos.php";
 require "../../src/Controllers/ProdutosControl.php";
 
-$nome = isset($_POST["nome"])?$_POST["nome"]:"[Nome não informado!]";
-$preco = isset($_POST["preco"])?$_POST["preco"]:"[Preço não informado!]";
+//$nome = isset($_POST["nome"])?$_POST["nome"]:"[Nome não informado!]";
+//$preco = isset($_POST["preco"])?$_POST["preco"]:"[Preço não informado!]";
 
 $cadastra = new ProdutosControl;
 
@@ -17,4 +17,11 @@ $produto = array(
 );
 
 $cadastra->insertProdutos('produtos', $produto);
+*/
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+$preco = filter_input(INPUT_POST, 'preco', FILTER_SANITIZE_SPECIAL_CHARS);
 
+echo json_encode($nome);
+echo json_encode($preco);
+
+echo json_encode('oi');
